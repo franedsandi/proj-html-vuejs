dfeca2511038e74821865c0b753126350cfdeec8
franedsandi <franedsandi@gmail.com>
2023-10-19 14:22:04 +0200

    [feat]header menu designed

--- src/components/patials/headermenu.vue
+++ src/components/patials/headermenu.vue
@@ -1,13 +1,51 @@
-<template>
-  <h1>holi</h1>
-</template>
-
 <script>
 export default {
 
 }
 </script>
 
-<style>
+<template>
+  <div class="container d-flex justify-content-between align-items-center">
+    <img src="img/mob-logo.png" alt="">
+    <ul class="d-flex gap-4">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Shop</a></li>
+      <li><a href="#">Blog</a></li>
+      <li><a href="#">Media</a></li>
+      <li><a href="#">ShortCode</a></li>
+      <li><a href="#">Features</a></li>
+    </ul>
+    <ul class="d-flex gap-4">
+      <li><a href="#"><i class="fa-solid fa-magnifying-glass"></i></a></li>
+      <li><a href="#"><i class="fa-regular fa-user"></i></a></li>
+      <li><a href="#"><i class="fa-solid fa-bag-shopping"></i></a></li>
+      <span>(0) &#163;0.00 </span>
+    </ul>
+  </div>
+</template>
 
+<style lang="scss" scoped>
+@import '../../scss/partials/vars';
+  .container{
+    height: 50px;
+    color: $white-color;
+    img{
+      width: 120px;
+    }
+    ul{
+      margin: 0;
+      list-style: none;
+      span{
+        color:$yellow-color;
+      }
+    }
+    a{
+      color: white;
+      text-decoration: none;
+      &:hover{
+        color: $yellow-color;
+      }
+    }
+
+  }
 </style>
