21dc549db4f54ab349f9fb6008369754f8efdf67
franedsandi <franedsandi@gmail.com>
2023-10-22 01:38:48 +0200

    [feat] policies dinamic

--- src/components/patials/policies.vue
+++ src/components/patials/policies.vue
@@ -1,61 +1,79 @@
 <script>
 export default {
-
-}
+  data() {
+    return {
+      items: [
+        {
+          image: 'img/caravan-white.png',
+          alt: 'caravan',
+          title: 'Free Delivery',
+          description: 'Free Shipping On All Orders',
+        },
+        {
+          image: 'img/bag-white.png',
+          alt: 'moneybag',
+          title: 'Money Return',
+          description: 'Back Guarentee in 7 days',
+        },
+        {
+          image: 'img/%-white.png',
+          alt: 'discount',
+          title: 'Member Discount',
+          description: 'On every order over $130.00',
+        },
+        {
+          image: 'img/back-white.png',
+          alt: 'moneyback',
+          title: 'Return Policy',
+          description: 'Support 24 hours a day',
+        },
+      ],
+    };
+  },
+  methods: {
+    changeImage(index) {
+      this.items[index].image = this.items[index].image.replace('-white', '-yellow');
+    },
+    resetImage(index) {
+      this.items[index].image = this.items[index].image.replace('-yellow', '-white');
+    },
+  },
+};
 </script>
 
 <template>
   <div class="container">
-    <ul class="d-flex align-items-center justify-content-between gap-4">
-      <li class="d-flex align-items-center">
-        <span><i class="fa-solid fa-truck-fast"></i></span>
+    <ul class="d-flex align-items-center gap-5">
+      <li v-for="(item, index) in items" :key="index" class="d-flex align-items-center" @mouseover="changeImage(index)" @mouseleave="resetImage(index)">
+        <span><img :src="item.image" :alt="item.alt"></span>
         <div class="info">
-          <p>Free Delivery</p>
-          <p>Free Shipping On All Orders</p>
-        </div>
-      </li>
-      <li class="d-flex align-items-center">
-        <span><i class="fa-solid fa-sack-dollar"></i></span>
-        <div class="info">
-          <p>Money Return</p>
-          <p>Back Guarentee in 7 days</p>
-        </div>
-      </li>
-      <li class="d-flex align-items-center">
-        <span><i class="fa-solid fa-percent"></i></span>
-        <div class="info">
-          <p>Member Discount</p>
-          <p>On every order over $130.00</p>
-        </div>
-      </li>
-      <li class="d-flex align-items-center">
-        <span><i class="fa-solid fa-hand-holding-dollar"></i></span>
-        <div class="info">
-          <p>Return Policy</p>
-          <p>Support 24 hours a day</p>
+          <p>{{ item.title }}</p>
+          <p>{{ item.description }}</p>
         </div>
       </li>
     </ul>
   </div>
 </template>
 
-
-
 <style lang="scss" scoped>
 @import '../../scss/partials/vars';
 
 .container {
+  
   ul {
     list-style: none;
     padding: 0px;
     margin: 0px 15px;
     height: 200px;
+    justify-content: center;
     li {
       position: relative;
       display: flex;
       align-items: center;
       padding-right: 10px;
-      width: 320px;
+      img{
+        height: 40px;
+      }
       span {
         color: white;
         font-size: 40px;
@@ -82,7 +100,7 @@ export default {
         position: absolute;
         top: 50%;
         transform: translateY(-50%);
-        right: 0;
+        right: -20px;
         height: 70%;
         width: 1px;
         background-color: grey;
